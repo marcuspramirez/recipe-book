dbdaa189784f91173940a8bf47a5a7f1230c809f
marcuspramirez <@macusramirez@gmail.com>
2023-02-18 18:49:55 -0600

    getting back to recipe app working

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Recipe One', 'Description Test', 'https://pinchofyum.com/cdn-cgi/image/width=360,height=514,fit=crop/wp-content/uploads/Couscous-Salad-1-3.jpg')
+  ];
+    
 
   constructor() { }
 
