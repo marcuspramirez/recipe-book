557d3cfd8251fce6cb2c857912f1ce3bffe2df74
marcuspramirez <@macusramirez@gmail.com>
2023-02-24 23:15:44 -0600

    add string int to recipe-list.component.html

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Recipe One', 'Description Test', 'https://pinchofyum.com/cdn-cgi/image/width=360,height=514,fit=crop/wp-content/uploads/Couscous-Salad-1-3.jpg')
+    new Recipe(
+      'Recipe One',
+      'Description Test',
+      'https://pinchofyum.com/cdn-cgi/image/width=360,height=514,fit=crop/wp-content/uploads/Couscous-Salad-1-3.jpg'
+    ),
+    new Recipe(
+      'Recipe One',
+      'Description Test',
+      'https://pinchofyum.com/cdn-cgi/image/width=360,height=514,fit=crop/wp-content/uploads/Couscous-Salad-1-3.jpg'
+    ),
   ];
-    
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
